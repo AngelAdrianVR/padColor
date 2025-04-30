4381dd29abd66ee47670fb762ed95de0238f3c17
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-04-29 22:45:24 -0600

    Producción
    • alerta en producción con info faltante

--- resources/js/Pages/Production/Tabs/Productions.vue
+++ resources/js/Pages/Production/Tabs/Productions.vue
@@ -31,7 +31,20 @@
                 <el-table v-else :data="productions" @row-click="handleRowClick" max-height="670" style="width: 100%"
                     class="mt-1" :row-class-name="tableRowClassName">
                     <!-- <el-table-column type="selection" width="45" /> -->
-                    <el-table-column prop="folio" label="N° Orden" />
+                    <el-table-column prop="folio" label="N° Orden">
+                        <template #default="scope">
+                            <div v-if="!scope.row.quantity" class="flex items-center space-x-2" :class="!scope.row.quantity ? 'text-red-600' : null">
+                                <span>{{ scope.row.folio }}</span>
+                                <el-tooltip content="Llenar información faltante" placement="top">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                                    </svg>
+                                </el-tooltip>
+                            </div>
+                        </template>
+                    </el-table-column>
                     <el-table-column prop="product.name" label="Producto" />
                     <el-table-column prop="product.season" label="Temporada" />
                     <el-table-column prop="quantity" label="Cantidad solicitada">
